2859b4c4c4cfa138e30bcf794218fb2d42ffa3cf
lequangtrieu <trieulqde160447@gmail.com>
2025-04-16 22:08:01 +0700

    update dummy data fe

--- src/App.js
+++ src/App.js
@@ -17,13 +17,22 @@ function App() {
     const password = localStorage.getItem("password");
     const token = localStorage.getItem("token");
 
+    const dummyUserDetail = {
+      id: 1,
+      username: "admin",
+      role: "ADMIN",
+      email: "admin",
+      isDeleted: false,
+    };
+
     if (username && password && token) {
       try {
-        const response = await axios.post(commonApi.userDetail.url, {
-          username,
-        });
-    
-        dispatch(setUserDetails(response.data.result));
+        // const response = await axios.post(commonApi.userDetail.url, {
+        //   username,
+        // });
+
+        // dispatch(setUserDetails(response.data.result));
+        dispatch(setUserDetails(dummyUserDetail));
       } catch (error) {
         if (error.response) {
           const { status, data } = error.response;
