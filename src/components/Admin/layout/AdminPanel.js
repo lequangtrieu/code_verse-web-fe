beef469a7d71a6dbd8a7e8938cf0423b5c6962ea
lequangtrieu <trieulqde160447@gmail.com>
2025-05-02 14:30:18 +0700

    fix respone ban user

--- src/components/Admin/layout/AdminPanel.js
+++ src/components/Admin/layout/AdminPanel.js
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Outlet, useNavigate } from "react-router-dom";
-
-import { Layout, Menu, Badge, message, Button } from "antd";
+import { Layout, Menu, Badge, message } from "antd";
 import {
   MenuUnfoldOutlined,
   MenuFoldOutlined,
